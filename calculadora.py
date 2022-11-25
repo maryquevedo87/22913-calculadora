258b83f646a22481334cb06e72e7d124b6e77c7a
maryquevedo87 <maryluzquevedo8@gmail.com>
2022-11-25 18:15:45 -0300

    1

--- calculadora.py
+++ calculadora.py
@@ -1,40 +1,126 @@
+import math
 import os
+import statistics
+from operaciones import*
 
-def sumar (n1, n2):
-    return n1+n2
-def restar (n1, n2):
-    return n1-n2
-def multiplicar(n1, n2):
-    return n1*n2
-def division (n1, n2):
-    try: 
-        return n1/n2
-    except:
-        print("no se puede dividir por 0")
-
-def menu ():
+def menu():
     os.system("cls")
     print("+ para sumar")
     print("- para restar")
-    print("* para multiplicar")
     print("/ para dividir")
+    print("* para multiplicar")
     print("s para salir")
 
+def menu_estadistica():
+    os.system("cls")
+    print("1 para media")
+    print("2 para mediana")
+    print("3 para moda")
+    print("4 para varianza")
+    print("5 para desviacion estandar")
+    print("6 para cargar datos")
+    print("7 para salir")
+
+def menu_cientifica():
+    os.system("cls")
+    print("1 para potencia")
+    print("2 para Raiz 2")
+    print("3 para seno")
+    print("4 para coseno")
+    print("5 para tangente")
+    print("6 para Salir")
+
+def cargar_datos(datos):
+    while True:
+        d=int(input("ingrese un dato: "))
+        datos.append(d)
+        print("desea ingresar otro dato? s/n")
+        op=input()
+        if op=="n":
+            break
+    return datos
 
 def main():
-    menu()
-    op=input("ingrese una operación ")
-    while op != "s":
-        pn=int(input("ingrese un número: "))
-        sn=int(input("ingrese otro número"))
-        if op=="+":
-            print ("el resultado de la suma es: " , sumar(pn,sn))
-        elif op=="-":
-             print ("el resultado de la resta es: " , restar(pn,sn))
-        elif op=="*":
-            print ("el resultado de la multiplicación es: " , multiplicar(pn,sn))
-        elif op=="/":
-             print ("el resultado de la división es: " , division(pn,sn))
-        else:
-            print("opción no válida")
-        op=input("ingrese una operación ")
+        
+    while True:
+        os.system("cls")
+        print("n Para calculadora normal")
+        print("e para datos estadisticos")
+        print("c Para calculadora cientifica")
+        print("s Para salir")
+        tc=input("ingrese el tipo de calculadora que desea usar: ")
+        if tc=="n":
+            menu()
+            op=input("ingrese una operación ")
+            while op != "s":
+                pn=int(input("ingrese un numero: "))
+                sn=int(input("ingrese otro numero: "))
+                if op=="+":
+                    print("el resultado de la suma es:" , sumar(pn,sn)) 
+                elif op =="-":
+                    print ("el resultado de la resta es: ",  restar(pn,sn))
+                elif op=="/":
+                    print("el resultado de la división es: ", dividir(pn, sn))
+                elif op=="*":
+                    print ("El resultado de multiplicar es:", multiplicar(pn,sn))
+                else:
+                    print("opcion no valida")
+                input("presione enter para continuar....")
+                menu()
+                op=input("ingrese una operación ")
+        elif tc=="e":
+            datos=[]
+            print("estadistica")
+            menu_estadistica()
+            op=input("ingrese una operación ")
+            while op != "7":
+                if op=="6":
+                    datos=cargar_datos(datos)
+                elif op=="1":
+                    if len(datos)>0:
+                        print("la media es: ",statistics.mean(datos))
+                    else:
+                        print("no hay datos cargados")
+                elif op=="2":
+                    if len(datos)>0:
+                        print("la mediana es: ", statistics.median(datos))
+                    else:
+                        print("no hay datos cargados")
+                elif op=="3":
+                    if len(datos)>0:
+                        print("la moda es: ", statistics.mode(datos))
+                    else:
+                        print("no hay datos cargados")
+                elif op=="4":
+                    if len(datos)>0:
+                        print("la varianza es: ", statistics.variance(datos))
+                    else:
+                        print("no hay datos cargados")
+                elif op=="5":
+                    if len(datos)>0:
+                        print("la desviacion estandar es: ", statistics.stdev(datos))
+                    else:
+                        print("no hay datos cargados")
+                else:
+                    print("opcion no valida")
+                input("presione enter para continuar....")
+                menu_estadistica()
+                op=input("ingrese una operación ")
+        elif tc=="c":
+            print("cientifica")
+            menu_cientifica()
+            op=input("seleccione la operacion: ")
+            if op=="1":
+                print("el resultado de la potencia es: ", math.exp())
+            elif op=="2":
+                print("el resultado de la raiz es: " , math.sqrt())
+            elif op=="3":
+                print("el resultado del logaritmo es: " , math.log())
+            elif op=="4":
+                print("el resultado del logaritmo en base 2 es: " , math.log2())
+            elif op=="5":
+                print("el resultado del logaritmo en base 10 es: " , math.log10())
+            input("ingrese enter para continuar")
+        elif tc=="s":
+            print("saliendo")
+            break
---
+++ operaciones.py
@@ -0,0 +1,51 @@
+#from tkinter import * # Importa el módulo
+#def centrar(ventana):
+#    ventana.update_idletasks()
+ #   w=ventana.winfo_width()
+#    h=ventana.winfo_height()
+#    extraW=ventana.winfo_screenwidth()-w
+#    extraH=ventana.winfo_screenheight()-h
+#    ventana.geometry("%dx%d%+d%+d" % (w,h,extraW/2,extraH/2))
+#def mostrar(ventana):
+#    ventana.deiconify()
+#def ocultar(ventana):
+#    ventana.withdraw()
+#def ejecutar(f): v0.after(200,f) # Una forma alternativa de ejecutar una función
+#v0 = Tk() # Tk() Es la ventana principal
+#v0.config(bg="black") # Le da color al fondo
+#v0.geometry("500x500") # Cambia el tamaño de la ventana
+#b1=Button(v0,text="ABRIR VENTANA V1",command=lambda:
+#    ejecutar(mostrar(v1))) # Primer botón
+#b1.grid(row=1,column=1) # El botón es cargado
+#b2=Button(v0,text="OCULTAR VENTANA V1",command=lambda:
+#    ejecutar(ocultar(v1))) # Segundo botón
+#b2.grid(row=1,column=2) # El botón es cargado
+#v1=Toplevel(v0) # Crea una ventana hija
+#v1.withdraw() # oculta v1
+#v0.mainloop() # Es el evento que llama al inicio de nuestro programa.
+#Siempre lo lleva la ventana principal.
+
+
+#  creo las funciones para las operaciones
+
+def sumar(n1,n2):
+    return n1 + n2
+def restar(n1, n2):
+    return n1-n2
+def multiplicar(n1, n2):
+    return n1*n2
+def dividir(n1,n2):
+    try:
+        return n1/n2
+    except:
+        print("no se puede dividir por cero ")
+def potencia():
+    n=int(input("ingrese el numero a elevar: "))
+    x=int (input("ingrese la ponencia: "))
+    i=1
+    r=n
+    while (i<x):
+        r= r * n
+        i=i+1
+    return r
+
